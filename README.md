2d3d8162f0cca01467c8be6be022304f2d7ef3be
David Alexander <dalexand@mymail.mines.edu>
2014-03-28 23:58:02 -0600

    fixed file locations. added simple database example

--- README.md
+++
@@ -1,4 +0,0 @@
-pex3
-====
-
-repository for advanced java project 3
---
+++ applet/JDBCMySQLDemo.java
@@ -0,0 +1,39 @@
+package applet;
+
+/**
+ * Demonstration of the most basic JDBC/MySQL connection.
+ * 
+ * @author Randy Bower
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCMySQLDemo
+{
+  public static void main( String args[] )
+  {
+    Connection con;
+    try
+    {
+      con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/sakila", "root", "" );
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery( "select * from film_text" );
+      
+      while( rs.next() )
+      {
+        System.out.println( rs.getString( "title" ) );
+      }
+      
+      rs.close();
+      stmt.close();
+      con.close();
+    }
+    catch( SQLException e )
+    {
+      e.printStackTrace();
+    }
+  }
+}
