620042942ba91ae1cf0f19c05065b57615afa7fd
David Alexander <dalexand@mymail.mines.edu>
2014-03-30 01:19:16 -0600

    image finally loads. MovieView panel now updates properly.

--- applet/HomePanel.java
+++ applet/HomePanel.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 
@@ -18,21 +19,30 @@ public class HomePanel
 	 */
 	
 	private Connection con;
+	private JFrame frame;
 	
 	
 	public HomePanel() {
 		setupConnection();
+		frame = new JFrame("test");
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
   public static void main( String args[] )
   {
-	  JFrame frame = new JFrame("test");
+	  /*JFrame frame = new JFrame("test");
 	  frame.setSize(500,500);
 	  frame.setVisible(true);
 	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	  frame.add(new QueueView());
-	  
+	  */
 	  HomePanel home = new HomePanel();
+	  //home.frame.add(new QueueView());
+	  home.frame.add(home.setupMovieView("ACE GOLDFINGER"));
+	  home.frame.validate();
+	  
 	  try
 	  {
 		  //con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/sakila", "root", "" );
@@ -41,7 +51,7 @@ public class HomePanel
       
 		  while( rs.next() )
 		  {
-			  System.out.println( rs.getString( "title" ) );
+			 // System.out.println( rs.getString( "title" ) );
 		  }
       
 		  rs.close();
@@ -50,11 +60,11 @@ public class HomePanel
 		  GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		  Font[] fonts = e.getAllFonts();
 		  for (Font f : fonts) {
-			  System.out.println(f.getFontName());
+			 // System.out.println(f.getFontName());
 		  }
 		  //MovieView test = new MovieView(home);
 		  //test.testPrint();
-		  home.gotoMovieView("Academy Dinosaur");
+		 // home.gotoMovieView("Academy Dinosaur");
 		  home.con.close();
     }
     catch( SQLException e )
@@ -68,17 +78,37 @@ public class HomePanel
    * 
    * @param title
    */
-  public void gotoMovieView(String title) {
-	  ResultSet rs = createResultSet("SELECT * FROM film_text WHERE title='" + title + "';");
-	  MovieView test = new MovieView(this, rs);
-	  test.testPrint();
+  public MovieView setupMovieView(String title) {
+	  HashMap<String, String> hm = new HashMap<>();
+	  ResultSet rs = createResultSet("SELECT * FROM nicer_but_slower_film_list WHERE title='" + title + "';");
+	  
+	  try {
+		  rs.first();
+		/*  System.out.println(rs.getString("title"));
+		  System.out.println(rs.getString("description"));
+		  System.out.println(rs.getString("category"));
+		  System.out.println(rs.getString("actors"));
+		  */
+		  hm.put("title", rs.getString("title"));
+		  hm.put("desc", rs.getString("description"));
+		  hm.put("genre", rs.getString("category"));
+		  hm.put("actors", rs.getString("actors"));
+	  } catch (SQLException e) {
+		  e.printStackTrace();
+	  }
+	  //frame.add( new MovieView(this, hm));
+	  //System.out.println("frame add");
+	 // frame.add(new QueueView());
+	  return new MovieView(this, hm);
+	  //test.testPrint();
   }
   
   public void gotoActorView(String actor) {
 	  
   }
   
-  public void gotoQueueView() {
+  public void gotoQueueView(String title) {
+	  //ResultSet rs = createResultSet(query)
 	  
   }
   
--- applet/MovieView.java
+++ applet/MovieView.java
@@ -1,42 +1,87 @@
 package applet;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 
-public class MovieView extends JPanel  {
-	
-	private HomePanel home;
-	private ResultSet rs;
-	
-	public MovieView(HomePanel home, ResultSet rs) {
-		this.home = home;
-		this.rs = rs;
-	}
-	
+public class MovieView extends JComponent implements MouseListener  {
 	/** 
 	 * To do:
 	 * print movie info
 	 * print list of actors
 	 * 
 	 */
-	public void testPrint() {
-		Statement stmt;
-		
+
+	private HomePanel home;
+	private HashMap<String, String> hm;
+	private ArrayList<String> actors;
+	private BufferedImage testImage;
+	
+	public MovieView(HomePanel home, HashMap<String, String> hm) {
+		this.home = home;
+		this.hm = hm;
+		actors = new ArrayList<String>(Arrays.asList(hm.get("actors").split(", ")));
+		addMouseListener(this);
+		testImage = null;
 		try {
-			//stmt = home.getCon().createStatement();
-			//ResultSet rs = stmt.executeQuery( "select * from film_text where title='Academy Dinosaur' " );
-			while (rs.next()) {
-				System.out.println(rs.getString("description"));
-			}
-			} catch (SQLException e) {
+			testImage = ImageIO.read( new File("images/cage1.jpg"));
+		}
+		catch (IOException e) {
 			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 		
+	
 	}
 	
 	
+	/**
+	 * Paints the movie info
+	 */
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		// swing fonts suck. So I chose aesthetically pleasing ones
+		g2d.setFont(new Font("Pescadero", Font.PLAIN, 30));
+		
+		// turn on antialiasing for sexy looking text
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,  
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);  
+		
+		g2d.drawString(hm.get("title"), 20, 20);
+		g2d.drawString("description", 100, 100);
+		
+		g2d.drawString("Your movie queue", 100, 100);
+		repaint();
+	}
 	
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	
+	private static final long serialVersionUID = 1L;
 }
